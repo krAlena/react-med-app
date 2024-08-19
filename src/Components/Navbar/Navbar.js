6492945baedea524da0646e43dd1dc14a0c8d170
krAlena <krasnoshchoka@bk.ru>
2024-08-19 14:47:28 +0300

    Navbar transform to React Cimponent

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Navbar.css';
+import '../../global.css';
+
+// import '../../public/img/health_care.png';
+
+const Navbar = () => {
+
+  return (
+    <div className="navigation-bar">
+      <div className="logo-part">
+        <div className="text-wrapper">StayHealthy</div>
+        <img className="illustration-health" src="/img/health_care.png"/>
+      </div>
+      <div className="tabs">
+        <div className="overlap-group">
+          <div className="tab active">
+            <div className="section">Home</div>
+          </div>
+          <div className="tab">
+            <div className="section">Appointments</div>
+          </div>
+          <div className="tab">
+            <div className="section">Health Blog</div>
+          </div>
+          <div className="tab">
+            <div className="section">Reviews</div>
+          </div>
+        </div>
+      </div>
+      <div className="login-btns">
+        <div className="primary"><div className="primary-2">Login</div></div>
+        <div className="primary-wrapper"><div className="primary-2">Sign-up</div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
