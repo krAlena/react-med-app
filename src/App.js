02bcdde32f38dceeba1c92f3cb66bc7d8ab1a4c2
krAlena <krasnoshchoka@bk.ru>
2024-08-20 14:18:14 +0300

    add default instant consultation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
+import InstantConsultation from './Components/Instant_Consultation/InstantConsultation';
 
 // Function component for the main App
 function App() {
@@ -26,6 +27,7 @@ function App() {
             <Route path="/" element={<Landing_Page/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<Sign_Up/>}/>
+            <Route path="/instant-consultation" element={<InstantConsultation/>}/>
             
           </Routes>
         </BrowserRouter>
